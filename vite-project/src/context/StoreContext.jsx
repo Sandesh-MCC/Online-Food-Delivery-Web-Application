13c54c240c48116ca2f186fd053370008759dc6c
Sandesh Kumar Mali <95668744+Sandesh-MCC@users.noreply.github.com>
2026-06-26 18:33:35 +0530

    Update API URL in StoreContext

--- vite-project/src/context/StoreContext.jsx
+++ vite-project/src/context/StoreContext.jsx
@@ -5,7 +5,7 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
-  const url = "http://localhost:4000";
+  const url = "https://online-food-delivery-web-application-fy5k.onrender.com";
   const [token, setToken] = useState("");
   const [food_list, setFoodList] = useState([]);
 
