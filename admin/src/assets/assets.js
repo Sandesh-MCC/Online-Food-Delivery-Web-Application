56de3912d881691698fb968cb979d01b8d651cb8
Sandesh Kumar Mali <95668744+Sandesh-MCC@users.noreply.github.com>
2026-06-26 18:35:46 +0530

    Add base URL for online food delivery application

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -5,6 +5,8 @@ import profile_image from "./profile_image.png";
 import upload_area from "./upload_area.png";
 import parcel_icon from "./parcel_icon.png";
 
+export const url="https://online-food-delivery-web-application-fy5k.onrender.com"
+
 export const assets = {
   eatfit_logo,
   add_icon,
